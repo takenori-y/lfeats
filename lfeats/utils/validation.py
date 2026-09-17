2e69ce63ecb1aad3c689d45c5e965ad7d12d4ce2
takenori-y <takenori.yoshimura24@gmail.com>
2026-09-17 14:21:07 +0900

    minor fix

--- lfeats/utils/validation.py
+++ lfeats/utils/validation.py
@@ -6,13 +6,15 @@
 from enum import Enum
 from typing import Any, TypeVar
 
+import numpy as np
 import torch
 import torch.nn.functional as F
 
-T = TypeVar("T", bound=Enum)
+EnumT = TypeVar("EnumT", bound=Enum)
+ArrayT = TypeVar("ArrayT", np.ndarray, torch.Tensor)
 
 
-def validate_enum(value: Any, enum_class: type[T], default: T) -> T:
+def validate_enum(value: Any, enum_class: type[EnumT], default: EnumT) -> EnumT:
     """Validate that the given value is a valid member of the specified enum class.
 
     Parameters
@@ -48,25 +50,27 @@ def validate_enum(value: Any, enum_class: type[T], default: T) -> T:
         ) from e
 
 
-def validate_length(x: torch.Tensor, min_length: int) -> torch.Tensor:
-    """Validate that the input tensor has at least the specified minimum length.
+def validate_length(x: ArrayT, min_length: int) -> ArrayT:
+    """Validate that the input array has at least the specified minimum length.
 
     Parameters
     ----------
-    x : torch.Tensor
-        The input tensor to validate.
+    x : np.ndarray | torch.Tensor
+        The input array to validate.
 
     min_length : int
-        The minimum expected length of the input tensor.
+        The minimum expected length of the input array.
 
     Returns
     -------
-    out : torch.Tensor
-        The input tensor, padded with zeros if its length is less than the minimum
-        length.
+    out : np.ndarray | torch.Tensor
+        The input array, padded with zeros at the end of the last axis if its length
+        is less than the minimum length.
 
     """
     actual_length = x.shape[-1]
-    if actual_length < min_length:
-        x = F.pad(x, (0, min_length - actual_length))
-    return x
+    if min_length <= actual_length:
+        return x
+    if isinstance(x, np.ndarray):
+        return np.pad(x, [(0, 0)] * (x.ndim - 1) + [(0, min_length - actual_length)])
+    return F.pad(x, (0, min_length - actual_length))
